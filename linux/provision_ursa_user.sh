c0a3f4ad22a507936bf07e65a504de35c964239a
Wes McKinney <wesm+git@apache.org>
2020-02-02 16:58:12 -0600

    Add machine provisioning script

---
+++ linux/provision_ursa_user.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# script idempotence
+userdel ursa
+rm -rf /home/ursa
+
+adduser --disabled-password --gecos "" ursa
+usermod -aG sudo ursa
+
+echo "ursa:ursa" | chpasswd
+
+SSH_DIR=/home/ursa/.ssh
+mkdir -p $SSH_DIR
+
+function add_ssh_keys() {
+    wget https://github.com/$1.keys -O ->> $SSH_DIR/authorized_keys
+}
+
+rm -f $SSH_DIR/authorized_keys
+
+add_ssh_keys bkietz
+add_ssh_keys fsaintjacques
+add_ssh_keys wesm
+add_ssh_keys kszucs
+add_ssh_keys jorisvandenbossche
+add_ssh_keys pitrou
+add_ssh_keys nealrichardson
+
+sudo chown -R ursa /home/ursa
